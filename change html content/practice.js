3678584e617e2fb9ea809253a3067794de40cd61
hithaishi-25 <g.hithaishi3818@gmail.com>
2024-06-05 12:18:36 +0530

    only the game files are kept in the folder

--- change html content/practice.js
+++
@@ -1,5 +0,0 @@
-function updatecontent(){
-    document.getElementById("example").innerHTML="content is modified as:";
-    document.getElementById("color").style.fontSize="50px";
-    document.getElementById("color").style.fontFamily="cursive";
-}
--- test.js
+++
@@ -1,6 +0,0 @@
-console.log("Hithaishi Vighnesh") ;
-console.log("together forever.")
-var x=2;
-var y=3;
-console.log("x is:",x+y);
-console.log("y is:"+ y+y);
